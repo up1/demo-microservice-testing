d54dfe72027f559b080ee47dafcb20a0b606dc75
up1 <somkiat.p@gmail.com>
2019-05-10 13:33:33 +0700

    Change file name of test

--- src/test/java/example/ExampleControllerIntegrationTest.java
+++ src/test/java/example/ExampleControllerIntegrationTest.java
@@ -21,7 +21,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = ExampleController.class)
-public class ExampleControllerAPITest {
+public class ExampleControllerIntegrationTest {
 
     @Autowired
     private MockMvc mockMvc;
